97b962f9746479f0c8182eb7980896511f543d97
JCataclisma <134896698+JCataclisma@users.noreply.github.com>
2026-08-07 06:52:09 -0300

    Delete JC_ScrapMap-v0.9.6 directory

--- JC_ScrapMap-v0.9.6/Start JC ScrapMap Overlay.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0launcher.ps1" -Overlay
-if errorlevel 1 (
-  echo.
-  echo JC ScrapMap overlay stopped with an error.
-  pause
-)
